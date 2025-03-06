9c70c2f0314eec92cf0780b2773a33ae030c6a32
VasaR <vasanthias52@gmail.com>
2025-03-07 01:09:33 +0530

    init

---
+++ server/src/types/index.d.ts
@@ -0,0 +1,39 @@
+import { Request } from "express";
+import { JwtPayload } from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string | JwtPayload;
+    }
+  }
+}
+
+export interface responseType {
+  success: boolean;
+  message: string;
+  error?: string;
+}
+
+export enum statusCode {
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  NOT_ACCEPTABLE = 406,
+  CONFLICT = 409,
+  SERVER_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+  BAD_GATEWAY = 502,
+  UNAVAILABLE = 503,
+  TIMEOUT = 504,
+}
+
+export interface CustomError extends Error {
+  status?: number;
+}
